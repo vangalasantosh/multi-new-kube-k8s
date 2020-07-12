126d44e1b6144915f16d63e692def264cee09185
vangalas <santosh.vangala-non-empl@moodys.com>
2020-07-12 17:05:44 -0400

    Kube First Deployment! Fix for Travis script

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@ docker build -t sri554278/multi-docker-client:latest -t sri554278/multi-docker-c
 docker build -t sri554278/multi-docker-server:latest -t sri554278/multi-docker-server:$SHA -f ./server/Dockerfile ./server
 docker build -t sri554278/multi-docker-worker:latest -t sri554278/multi-docker-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push vangalasantosh06/multi-docker-client:latest
-docker push vangalasantosh06/multi-docker-client:$SHA
-docker push vangalasantosh06/multi-docker-server:latest
-docker push vangalasantosh06/multi-docker-server:$SHA
-docker push vangalasantosh06/multi-docker-worker:latest
-docker push vangalasantosh06/multi-docker-worker$SHA
+docker push sri554278/multi-docker-client:latest
+docker push sri554278/multi-docker-client:$SHA
+docker push sri554278/multi-docker-server:latest
+docker push sri554278/multi-docker-server:$SHA
+docker push sri554278/multi-docker-worker:latest
+docker push sri554278/multi-docker-worker$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=vangalasantosh06/multi-docker-server:$SHA
-kubectl set image deployments/client-deployment client=vangalasantosh06/multi-docker-client:$SHA
-kubectl set image deployments/worker-deployment worker=vangalasantosh06/multi-docker-worker:$SHA
+kubectl set image deployments/server-deployment server=sri554278/multi-docker-server:$SHA
+kubectl set image deployments/client-deployment client=sri554278/multi-docker-client:$SHA
+kubectl set image deployments/worker-deployment worker=sri554278/multi-docker-worker:$SHA
